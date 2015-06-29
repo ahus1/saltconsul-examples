c8e166e9d1300eacdea9b0f04282a5bc015a3fea
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-06-29 16:03:33 +0200

    trying to fix directories

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -1,7 +1,10 @@
+set -x
+set -e
 mkdir -p $HOME/pages/tutorial
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 cp */*.png $HOME/pages/tutorial
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
-ls
+ls -R
+ls -R $HOME/pages
 mv *.png output $HOME/pages/tutorial
