57fcdfd9a7882c36988f090800ba51f9da9dd9cc
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-06-29 15:44:50 +0200

    fixing paths for images

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -1,5 +1,8 @@
+set -e
+set -o pipefail
 mkdir -p $HOME/pages/tutorial
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 cp */*.png $HOME/pages/tutorial
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
+mv *.png output $HOME/pages/tutorial
