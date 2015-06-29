00b67ff306367248478870650571f263c9c03fe4
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-06-29 15:50:17 +0200

    fewer options for travis

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -1,8 +1,7 @@
-set -e
-set -o pipefail
 mkdir -p $HOME/pages/tutorial
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 cp */*.png $HOME/pages/tutorial
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
+ls
 mv *.png output $HOME/pages/tutorial
