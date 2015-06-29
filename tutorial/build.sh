8406b2b8755d4c350849516d40d61f3a5f698b51
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-06-29 16:13:53 +0200

    found path for generated image

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -5,6 +5,4 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 cp */*.png $HOME/pages/tutorial
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
-ls -R
-ls -R $HOME/pages
-mv *.png output $HOME/pages/tutorial
+mv tutorial/*.png $HOME/pages/tutorial
