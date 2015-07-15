238bfe6645fca4caf3d03574911d00d3bd6cd013
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-07-15 22:48:11 +0200

    updating paths

--- tutorial/build.bat
+++ tutorial/build.bat
@@ -1,5 +1,6 @@
 call asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D output
-copy 20_getting_started\*.png output
-copy 45_setup_monitoring\*.png output
-move *.png output
-move *.png.cache output
+mkdir output\tutorial
+copy 20_getting_started\*.png output\tutorial
+copy 45_setup_monitoring\*.png output\tutorial
+move tutorial\*.png output\tutorial
+move tutorial\*.png.cache output\tutorial
