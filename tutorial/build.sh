f3e724b6db7f042185b154da6c986403e368af63
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-08-18 17:15:50 +0200

    fixing location of images

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -5,4 +5,4 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 cp */*.png $HOME/pages/tutorial
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
-mv tutorial/*.png $HOME/pages/tutorial
+mv */*.png $HOME/pages/tutorial
