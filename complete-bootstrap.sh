303393bbb24bac367d8b0031f92f361f9f99e420
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-10-22 17:20:08 +0200

    reorder steps in bootstrapping

--- complete-bootstrap.sh
+++ complete-bootstrap.sh
@@ -5,5 +5,5 @@ salt-key --gen-keys=minion --gen-keys-dir=/etc/salt/pki/minion
 mkdir -p /etc/salt/pki/minion
 cp /etc/salt/pki/minion/minion.pub /etc/salt/pki/master/minions/master
 service salt-master start
-service salt-minion start
 salt-call -l debug state.highstate
+service salt-minion start
